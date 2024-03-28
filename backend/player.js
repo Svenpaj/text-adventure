7da64d9b6f0d08f944b162a66cf01dd9c959d8b1
Svenpaj <114157351+Svenpaj@users.noreply.github.com>
2024-03-28 21:23:15 +0100

    added a few enemies

--- backend/player.js
+++ backend/player.js
@@ -1,4 +1,16 @@
 let player = {
     name: 'Player 1',
     life: 100,
+    attack: 10,
+    defend: 5,
+    speed: 5,
+    level: 1,
+    experience: 0,
+    nextLevel: 100,
+    inventory: [],
+    equip: [],
+    skills: [],
+    quests: [],
 }
+
+export { player }
