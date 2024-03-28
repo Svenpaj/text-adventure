abf29f6849c1234b276c7e88e277986c6f36b5c3
Svenpaj <114157351+Svenpaj@users.noreply.github.com>
2024-03-28 11:51:17 +0100

    some fun stuff

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3001;
+const path = require('path');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+/*app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});*/
 
 app.listen(port, () => {
     console.log(`app listening at http://localhost:${port}`);
