6146c6ab24dddcbe348aab3d54cc50ce3d87d84f
Svenpaj <114157351+Svenpaj@users.noreply.github.com>
2024-04-08 01:43:16 +0200

    .

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 // server.js
-import express from 'express';
+import express, { query, response } from 'express';
 import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
+import { dirname, join, parse } from 'path';
 import openDB from './database.js';
 import bcrypt from 'bcrypt';
 
@@ -16,6 +16,7 @@ app.use(express.urlencoded({ extended: true })); // To parse URL-encoded bodies)
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const db = await openDB();
 
 //app.use(express.static('frontend'));
 //app.use(express.json()); // To parse JSON bodies
@@ -29,26 +30,26 @@ app.post('/api/register', async (req, res) => {
     if (!username || !password) {
         return res.status(400).send('Username and password are required');
     }
-    bcrypt.hash(password, saltRounds, function (err, hash) {
+
+    bcrypt.hash(password, saltRounds, async function (err, hash) {
         if (err) {
             console.error(err.message);
-            return res.status(500).send('Server error');
+            return res.status(500).send('Failed to register the user');
+        }
+        try {
+            const query = `INSERT INTO users (username, password) VALUES (?, ?)`;
+            const result = await db.run(query, [username, password]);
+            console.log(`A new row has been inserted with rowid ${result.lastID}`);
+            res.send('User registered successfully');
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Failed to register the user');
         }
     });
-
-    const db = await openDB();
-    try {
-        const query = `INSERT INTO users (username, password) VALUES (?, ?)`;
-        const result = await db.run(query, [username, password]);
-        console.log(`A new row has been inserted with rowid ${result.lastID}`);
-        res.send('User registered successfully');
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Failed to register the user');
-    }
 });
 
 
+// fix the login route / register route
 app.get('/login', (req, res) => {
     res.sendFile(join(__dirname, '../frontend/login.html'));
 });
@@ -62,6 +63,13 @@ app.get('/game', (req, res) => {
 app.post('/api/login', (req, res) => {
     console.log(`logged in: `, req.body);
     res.redirect('/game');
+});
+
+// Parse the data from the database into JSON
+app.get('/api/users', (req, res) => {
+    const query = 'SELECT * FROM users';
+    const result = db.run(query);
+    res.json(result);
 });
 
 app.use(express.static('frontend'));
